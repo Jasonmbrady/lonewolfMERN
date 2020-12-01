01d6fbeed7d894034692701940cc17be9987a074
Jason Brady <jasonmbrady42@gmail.com>
2020-12-01 03:27:36 -0800

    Character model

--- server/models/Character.js
+++ server/models/Character.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 
 const CharacterSchema = new mongoose.Schema({
     name: String,
-
+    weapons: Array,
+    backpack: Array,
+    misc: Array,
+    disciplines: Array,
+    beltpouch: Number,
+    currEnd: Number,
+    maxEnd: Number,
+    combatSkill: Number
 
 });
 
